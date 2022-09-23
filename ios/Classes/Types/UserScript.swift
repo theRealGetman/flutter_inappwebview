31a5561c43389e68b100e10f41207a4ba4bedcfb
Getman <a.e.getman@gmail.com>
2022-09-23 11:30:52 +0300

    Fix for XCode 14

--- ios/Classes/Types/UserScript.swift
+++ ios/Classes/Types/UserScript.swift
@@ -10,18 +10,29 @@ import WebKit
 
 public class UserScript : WKUserScript {
     var groupName: String?
+
+    private var contentWorldWrapper: Any?
     @available(iOS 14.0, *)
-    lazy var contentWorld: WKContentWorld = WKContentWorld.page
-    
+    var contentWorld: WKContentWorld {
+      get {
+        if let value = contentWorldWrapper as? WKContentWorld {
+          return value
+        }
+        return .page
+      }
+      set { contentWorldWrapper = newValue }
+    }
+
+
     public override init(source: String, injectionTime: WKUserScriptInjectionTime, forMainFrameOnly: Bool) {
         super.init(source: source, injectionTime: injectionTime, forMainFrameOnly: forMainFrameOnly)
     }
-    
+
     public init(groupName: String?, source: String, injectionTime: WKUserScriptInjectionTime, forMainFrameOnly: Bool) {
         super.init(source: source, injectionTime: injectionTime, forMainFrameOnly: forMainFrameOnly)
         self.groupName = groupName
     }
-    
+
     @available(iOS 14.0, *)
     public override init(source: String, injectionTime: WKUserScriptInjectionTime, forMainFrameOnly: Bool, in contentWorld: WKContentWorld) {
         super.init(source: source, injectionTime: injectionTime, forMainFrameOnly: forMainFrameOnly, in: contentWorld)
@@ -34,12 +45,12 @@ public class UserScript : WKUserScript {
         self.groupName = groupName
         self.contentWorld = contentWorld
     }
-    
+
     public static func fromMap(map: [String:Any?]?, windowId: Int64?) -> UserScript? {
         guard let map = map else {
             return nil
         }
-        
+
         let contentWorldMap = map["contentWorld"] as? [String:Any?]
         if #available(iOS 14.0, *), let contentWorldMap = contentWorldMap {
             let contentWorld = WKContentWorld.fromMap(map: contentWorldMap, windowId: windowId)!
